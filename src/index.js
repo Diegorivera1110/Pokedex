d2dae94d031b536ddde0cd437eb9c1d33db9dc45
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-08-19 15:39:59 -0700

    updated imports in index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Reac from "react"
+import ReactDOM from "react-dom/client"
+import './index.css'
+import App from './App'
+import store from './app/store'
+import { Provider } from 'react-redux'
+
