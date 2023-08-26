e445eeff13540e95c735a0f8c2d524a1e5894e79
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-08-26 13:11:53 -0700

    export function started

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -2,3 +2,10 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decremenl, increment } from './counterSlice'
 import  styles from './Counter.module.css'
+
+export function Counter() {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
+
+    
+}
