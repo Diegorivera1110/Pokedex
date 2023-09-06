e6d48fbec60c7b2ee53384b026aa7d663b08f36a
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-09-05 19:54:05 -0700

    added components imports to app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
 import React, {useState, useEffect } from "react";
 import "./index.css";
 
+import About from './components/About';
+import Error from './components/Error';
+import Favorites from './components/Favorites';
+import Filter from './components/Filter';
+import footer from './components/Footer';
+import Header from './components/Header';
+import LoadingSpinner from './components/LoadingSpinner';
+import Modal from './components/Modal';
+// import Navigation from './components/'
+import Notifications from './components/Notifications';
+import PokeCard from './components/PokeCard';
+import PokeDetail from './components/PokeDetail';
+import PokeList from './components/PokeList';
+import Search from './components/Search';
