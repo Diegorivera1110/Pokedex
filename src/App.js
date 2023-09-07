b2877eaf07661183c4cfb3d6e50eb56b07befc83
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-09-06 18:36:22 -0700

    added app function to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect } from "react";
-import "./index.css";
 
 import About from './components/About';
 import Error from './components/Error';
@@ -15,3 +14,34 @@ import PokeCard from './components/PokeCard';
 import PokeDetail from './components/PokeDetail';
 import PokeList from './components/PokeList';
 import Search from './components/Search';
+
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    navigate
+} from 'react-router-dom';
+
+function App() {
+    const[load, updateLoad] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            updateLoad(false);
+        }, 1200);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <Router>
+            
+        </Router>
+
+    );
+}
+
+export default App;
