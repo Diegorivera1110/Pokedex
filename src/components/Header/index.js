bb99f49e66f2fa1ff3908981e6d021a0b399a64c
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-09-11 06:08:58 -0700

    imported components into Header, added page render to Header funtion

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Navigation from "../Navigation";
+import About from "../About/index";
+import Favorites from "../Favorites/index";
+import Notifications from "../Notifications/index";
+import PokeCard from "../PokeCard/index";
+import PokeList from "../PokeList/index";
+import Search from "../Search/index";
+
+function Header() {
+    const [currentPage, pageHandler] = useState("About");
+
+    const renderPage = () => {
+        switch (currentPage) {
+
+        }
+    };
+
+    return (
+        <div>
+            <Navigation currentPage={currentPage} pageHandler={pageHandler} />
+            <div>{renderPage()}</div>
+        </div>
+    )
+} 
+
+export default Header;
