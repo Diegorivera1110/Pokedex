32f1e5ebfa77bb1a13a98facd4e1f7747b897fbc
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-09-14 20:18:16 -0700

    created Modal funtion

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,1 +1,17 @@
 import React from "react";
+import { connect } from 'react-redux';
+import {  closeModal } from '../../redux/actions';
+
+const Modal = ({ isOpen, closeModal, modalContent }) => {
+    if (!isOpen) {
+        return null;
+    }
+
+    return (
+        <div className="modal">
+            <div className="modal-content">
+
+            </div>
+        </div>
+    );
+};
