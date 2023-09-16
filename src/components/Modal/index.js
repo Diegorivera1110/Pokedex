676159ce3f087929f6f666cc4a057b4035c333aa
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-09-15 20:42:58 -0700

    added mapDispatch & mapState to modal component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -10,8 +10,22 @@ const Modal = ({ isOpen, closeModal, modalContent }) => {
     return (
         <div className="modal">
             <div className="modal-content">
-
+                <button className="close-button" onClick={closeModal}>
+                    Close
+                </button>
+                {modalContent}
             </div>
         </div>
     );
 };
+
+const mapStateToProps = (state) => ({
+    isOpen: state.modal.isOpen,
+    modalContent: state.modal.content,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    closeModal: () => dispatch(closeModal()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Modal);
