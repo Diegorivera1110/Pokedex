e4d5189006619ff71f7bb7b8a607342f33dd3b41
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-09-16 14:56:56 -0700

    added modal actions to actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,17 @@
 export const FETCH_POKEMON_SUCCESS = 'FETCH_POKEMON_SUCCESS';
+export const OPEN_MODAL = 'OPEN_MODAL';
+export const CLOSE_MODAL = 'CLOSE_MODAL';
 
 export const fetchPokemonSuccess = (pokemonData) => ({
     type: FETCH_POKEMON_SUCCESS,
     payload: pokemonData,
 })
+
+export const openModal = (content) => ({
+    type: OPEN_MODAL,
+    payload: content,
+});
+
+export const closeModal = () => ({
+    type: CLOSE_MODAL,
+});
