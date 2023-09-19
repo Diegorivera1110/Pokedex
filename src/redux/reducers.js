b68a990bd37b4cb437f2946e748c1ef44c426e3c
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-09-18 20:39:27 -0700

    added multiple reducers to reducers.js

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,12 @@
-import { FETCH_POKEMON_SUCCESS } from "./actions";
+import { FETCH_POKEMON_SUCCESS, OPEN_MODAL, CLOSE_MODAL } from "./actions";
+
+import { createStore, combineReducers } from "redux";
+import { pokemonReducer, modalReducer } from "./reducers";
 
 const initialState = {
     pokemon: [],
+    isOpen: false,
+    content: null,
 };
 
 const pokemonReducer = (state = initialState, action) => {
@@ -13,4 +18,27 @@ const pokemonReducer = (state = initialState, action) => {
     }
 };
 
-export default pokemonReducer;
+const modalReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case OPEN_MODAL:
+            return {
+                ...state,
+                isOpen: true,
+                content: action.payload,
+            };
+            case CLOSE_MODAL:
+                return initialState;
+                default:
+                    return state;
+    }
+};
+
+
+const rootReducer = combineReducers({
+    pokemon: pokemonReducer,
+    modal: modalReducer,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
